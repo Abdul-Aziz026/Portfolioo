85c54dbbf677acd0008ecd31e78ab9987660e0fb
Abdul Aziz <azizulcsebsmrstu@gmail.com>
2024-05-15 00:05:34 +0600

    portfolio Web Initial upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".text", {
+    strings: ["Competitive Programmar", "Problem-Solver", "Candidate-Master on Codeforces", "5-star on Codechef", "2-Times Asia West Continental Finalist"],
+    typeSpeed: 100,
+    backSpeed: 20,
+    backDelay: 1000,
+    loop: true
+});
