fece645a350cb7623420c22f61107bd636f86023
Abdul Aziz <azizulcsebsmrstu@gmail.com>
2024-05-17 20:01:03 +0600

    solve count + about update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let typed = new Typed(".text", {
-    strings: ["Fresh graduate Student", "Competitive Programmar", "Candidate-Master on Codeforces", "5-star Coder on Codechef", "2-Times Asia West Continental Finalist", "Problem-Solver"],
+    strings: ["Fresh graduate Student", "Competitive Programmar", "Candidate-Master on Codeforces", "5-star Coder on Codechef", "2-Times Asia West Continental Finalist", "passionate Problem-Solver"],
     typeSpeed: 100,
     backSpeed: 20,
     backDelay: 1000,
